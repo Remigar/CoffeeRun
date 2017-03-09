5016a2ba5ec81a765205d661363a6d087615e757
Eric Lara <elara9000@gmail.com>
2017-03-08 20:50:05 -0800

    Update tests.js

--- scripts/tests.js
+++ scripts/tests.js
@@ -15,7 +15,7 @@ QUnit.test('figure 8.10 code tests', function(assert) {
     })
     ds.remove('james@bond.com');
     assert.propEqual(ds.getAll(), {
-        'm@bond.com': "tea"
+        'm@bond.com': 'tea'
     });
     assert.equal(ds.get('m@bond.com'), 'tea');
     assert.equal(ds.get('james@bond.com'), undefined);
@@ -37,8 +37,8 @@ QUnit.test('figure 8.32 code tests', function(assert) {
     });
     /*printOrders() doesnt actually return anything, so truck.js had to be changed
     to return a string for the tests to work correctly*/
-    assert.equal(myTruck.printOrders(), 'Truck #ncc-1701 has pending orders:\nme@goldfinger.com: double mocha\ndr@no.com: decaf\nm@bond: earl grey\n');
-    myTruck.deliverOrder('dr@no.com);
+    assert.equal(myTruck.printOrders(), 'Truck #ncc-1701 has pending orders:\nme@goldfinger.com: double mocha\ndr@no.com: decaf\nm@bond.com: earl grey\n');
+    myTruck.deliverOrder('dr@no.com');
     myTruck.deliverOrder('m@bond.com');
     assert.equal(myTruck.printOrders(), 'Truck #ncc-1701 has pending orders:\nme@goldfinger.com: double mocha\n');
 });
